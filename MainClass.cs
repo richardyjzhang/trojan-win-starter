6bd8ba33b42bfb00813330c23051ab61705a6059
zhangyj <richard.yujia.zhang@gmail.com>
2020-03-05 08:59:53 +0800

    就这几行代码…

---
+++ MainClass.cs
@@ -0,0 +1,15 @@
+﻿namespace trojan_win_starter
+{
+  static class MainClass
+  {
+    static void Main()
+    {
+      System.Diagnostics.Process process = new System.Diagnostics.Process();
+      process.StartInfo.FileName = "./trojan.exe";
+      process.StartInfo.Arguments = "./config.json";
+      process.StartInfo.CreateNoWindow = true;
+      process.StartInfo.UseShellExecute = false;
+      process.Start();
+    }
+  }
+}
